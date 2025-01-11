e9c9e5148048161c04c1c59c14d3ee1125485169
samuruddhi07 <samruddhimane2907@gmail.com>
2025-01-11 16:17:14 +0530

    Add files via upload

---
+++ assignment1.php
@@ -0,0 +1,80 @@
+<html>
+<body>
+    <?php
+    //string operations
+
+
+    //a.concatation
+    $firstname="samruddhi";
+    $lastname="mane";
+    $fullname=$firstname."".$lastname;
+    
+    //b.string length
+    $str="hello,world!";
+    echo strlen($str);
+
+    //c.substring extraction
+    $str="hello,world!";
+    echo substr($str,0,5);
+
+    //d.case conversion
+    $text="simmba the king";
+    echo strtoupper($text);                                         //converts to uppercase
+    echo strtolower($text);                                         //converts to lowercase
+    echo ucfirst($text);                                            //capstalizes the first letter
+    echo ucwords($text);                                            //capatalizes the first letter if each world
+
+    //e.trimming
+    $str=" friends ";
+    echo trim($str);                                             //removes whitespace
+    echo itrim($str);                                            //removes from the left
+    echo rtrim($str);                                           //removes from the right 
+
+    //f.reverse a string
+    $str="hello,world!";
+    echo strrev($str);
+
+    //g.compares two strings(case-sensitive)
+    $str="hello,world!";
+    echo strcmp($str);
+
+    //h.compares two strings(case-insensitive)
+    $str="hello,world!";
+    echo strcasecmp($str);
+
+
+    //numeric operations
+
+
+    //a.arithmetic operations
+    $a=10;
+    $b=3;
+    echo $a+$b;
+    echo $a-$b;
+    echo $a*$b;
+    echo $a/$b;
+    echo $a%$b;
+    echo $a**$b;
+    
+    //b.rounds up to the nearest integer
+    echo ceil(6.8);
+
+    //c.rounds down to the nearest integer
+    echo floor(6.8);
+
+    //d.returns the square root
+    echo sqrt(25);
+
+    //e.generates a random number between $min and $max
+    echo rand(10,50);
+
+    //f.checks if a value is numeric or not
+    echo is_numeric(36);
+?>
+</body>
+</html>
+
+
+
+
+
