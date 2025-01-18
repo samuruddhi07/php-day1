8769a085df7438165b669e1fedbbbce7331dea99
samuruddhi07 <samruddhimane2907@gmail.com>
2025-01-18 21:07:40 +0530

    Update Q4part1.php

--- Q4part1.php
+++ Q4part1.php
@@ -7,16 +7,16 @@
     <h2>user feedback form</h2>
     <form action="submit.php"method="post">
 
-        <label for="name">Name:</lable>
+        <label for="name">Name:</lable><br>
         <input type="text"id="name"name="name"required><br><br>
 
-        <lable for="mobile">Mobile Number:</lable>
+        <lable for="mobile">Mobile Number:</lable><br>
         <input type="tel"id="mobile"name="mobile"required><br><br>
 
-        <lable for="email">Email:</label>
+        <lable for="email">Email:</label><br>
         <input type="email"id="email"name="email"required><br><br>
 
-        <lable for="message">Message:</lable>
+        <lable for="message">Message:</lable><br>
         <textarea id="message"name="message"required></textarea><br><br>
 
         <input type="submit"value=" Submit ">
