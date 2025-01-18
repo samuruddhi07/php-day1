fb353c9d4a1c9d5d0762fac9b34157c583e04761
samuruddhi07 <samruddhimane2907@gmail.com>
2025-01-18 20:35:25 +0530

    assignment 2

---
+++ Q4part1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>user feedback form</title>
+</head>
+<body>
+    <h2>user feedback form</h2>
+    <form action="submit.php"method="post">
+
+        <label for="name">Name:</lable>
+        <input type="text"id="name"name="name"required><br><br>
+
+        <lable for="mobile">Mobile Number:</lable>
+        <input type="tel"id="mobile"name="mobile"required><br><br>
+
+        <lable for="email">Email:</label>
+        <input type="email"id="email"name="email"required><br><br>
+
+        <lable for="message">Message:</lable>
+        <textarea id="message"name="message"required></textarea><br><br>
+
+        <input type="submit"value=" Submit ">
+</form>
+</body>
+</html>
